093b607c77aa747eb0a4dad065335ae1e1beb40c
faiyamm <sofia.almaraz@uabc.edu.mx>
2026-03-16 19:02:40 -0700

    Initial Commit

---
+++ HealthTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HealthTracker
+//
+//  Created by Fai on 16/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HealthTracker/HealthTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackerApp.swift
+//  HealthTracker
+//
+//  Created by Fai on 16/03/26.
+//
+
+import SwiftUI
+
+@main
+struct HealthTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
